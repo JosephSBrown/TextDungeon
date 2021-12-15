1b7c7731035ebcc007355090e612c9fb2c43b7f6
Joseph Brown <84574944+JosephSBrown@users.noreply.github.com>
2021-12-15 02:40:52 +0000

    Commented Up Code

--- Main.py
+++ Main.py
@@ -1,106 +1,114 @@
+##import of standard modules
 from os import write
 import tkinter as tk
 from tkinter import Label, ttk
 from tkinter.constants import BOTH, LEFT
 
+##import of game related classes
 from Game import Game
 
+##login class
 class Login():
 
+##initialising window
     def __init__(self):
         self.Window()
 
+##Login Window Function
     def Window(self):
-        self.App = tk.Tk()
-        self.App.title('Text Dungeon | Login')
-        self.App.geometry('1040x510')
+        self.App = tk.Tk() #Creating Tkinter Window
+        self.App.title('Text Dungeon | Login') #Setting Window Title
+        self.App.geometry('1040x510') #Setting Window Resolution
 
-        CreateFrame = ttk.Frame(self.App)
-        LoginFrame = ttk.Frame(self.App)
+        CreateFrame = ttk.Frame(self.App) #Creating frame for profile creation widgets
+        LoginFrame = ttk.Frame(self.App) #Creating frame for login widgets
 
-        CreateFrame.pack(side='left', fill='both', expand=True)
-        LoginFrame.pack(side='right', fill='both', expand=True)
+        CreateFrame.pack(side='left', fill='both', expand=True) #packing the frames
+        LoginFrame.pack(side='right', fill='both', expand=True) #packing the frames
 
-        UserLabel = tk.Label(CreateFrame, text='Please Choose a Username')
-        self.CreateUsername = tk.Entry(CreateFrame, width=30)
-        PassLabel = tk.Label(CreateFrame, text='Please Choose a Password')
-        self.CreatePassword = tk.Entry(CreateFrame, width=30, show='*')
-        ConfirmLabel = tk.Label(CreateFrame, text='Please Confirm Password')
-        self.ConfirmUsername = tk.Entry(CreateFrame, width=30, show='*')
-        Confirm = tk.Button(CreateFrame, text='Confirm', command=self.RunCreate)
-        self.CreateError = tk.Label(CreateFrame, text=" ")
+        UserLabel = tk.Label(CreateFrame, text='Please Choose a Username') #configuring create username label
+        self.CreateUsername = tk.Entry(CreateFrame, width=30) #configuring create username entry box
+        PassLabel = tk.Label(CreateFrame, text='Please Choose a Password') #configuring create password label
+        self.CreatePassword = tk.Entry(CreateFrame, width=30, show='*') #configuring create password entry box
+        ConfirmLabel = tk.Label(CreateFrame, text='Please Confirm Password') #configuring password confirmation label
+        self.ConfirmUsername = tk.Entry(CreateFrame, width=30, show='*') #configuring password confirmation entrybox
+        Confirm = tk.Button(CreateFrame, text='Confirm', command=self.RunCreate) #configuring confirm button
+        self.CreateError = tk.Label(CreateFrame, text=" ") #configuring creation error message
 
-        UsernameLabel = tk.Label(LoginFrame, text='Enter Username')
-        self.Username = tk.Entry(LoginFrame, width=30)
-        PasswordLabel = tk.Label(LoginFrame, text='Enter Password')
-        self.Password = tk.Entry(LoginFrame, width=30, show='*')
-        Submit = tk.Button(LoginFrame, text='Login', command=self.RunLogin)
-        self.ErrorLabel = tk.Label(LoginFrame, text=" ")
+        UsernameLabel = tk.Label(LoginFrame, text='Enter Username') #configuring login username label
+        self.Username = tk.Entry(LoginFrame, width=30) #configuring login username entry box
+        PasswordLabel = tk.Label(LoginFrame, text='Enter Password') #configuring login password label
+        self.Password = tk.Entry(LoginFrame, width=30, show='*') #configuring password entry box
+        Submit = tk.Button(LoginFrame, text='Login', command=self.RunLogin) #configuring submit button
+        self.ErrorLabel = tk.Label(LoginFrame, text=" ") #configuring error label
 
-        UserLabel.grid(row=0, column=0)
-        self.CreateUsername.grid(row=1, column=0)
-        PassLabel.grid(row=2, column=0)
-        self.CreatePassword.grid(row=3, column=0)
-        ConfirmLabel.grid(row=4, column=0)
-        self.ConfirmUsername.grid(row=5, column=0)
-        Confirm.grid(row=6, column=0)
-        self.CreateError.grid(row=7, column=0)
+        UserLabel.grid(row=0, column=0) #laying out user creation label
+        self.CreateUsername.grid(row=1, column=0) #laying out user creation box
+        PassLabel.grid(row=2, column=0) #laying out user password label
+        self.CreatePassword.grid(row=3, column=0) #laying out user password box
+        ConfirmLabel.grid(row=4, column=0) #laying out user password confirm label
+        self.ConfirmUsername.grid(row=5, column=0) #laying out user password confirm box
+        Confirm.grid(row=6, column=0) #laying out confirm button
+        self.CreateError.grid(row=7, column=0) #laying out creation error label
 
-        UsernameLabel.grid(row=0, column=0)
-        self.Username.grid(row=1, column=0)
-        PasswordLabel.grid(row=2, column=0)
-        self.Password.grid(row=3, column=0)
-        Submit.grid(row=4, column=0)
-        self.ErrorLabel.grid(row=5, column=0)
+        UsernameLabel.grid(row=0, column=0) #laying out user login label
+        self.Username.grid(row=1, column=0) #laying out user login entry
+        PasswordLabel.grid(row=2, column=0) #laying out user login password label
+        self.Password.grid(row=3, column=0) #laying out user login password entry
+        Submit.grid(row=4, column=0) #laying out submit button
+        self.ErrorLabel.grid(row=5, column=0) #laying out login error message
 
-        self.App.mainloop()
+        self.App.mainloop() #Activating Tkinter Window, putting it in loop
 
+##Login Script for Existing Users
     def RunLogin(self):
-        UserID = self.Username.get()
-        self.UserPass = self.Password.get()
-        UserFile = UserID + '.txt'
+        UserID = self.Username.get() #get username entry and set variable
+        self.UserPass = self.Password.get() #get password entry and set variable
+        UserFile = UserID + '.txt' #setting the variable for File Name
 
-        try: 
-            with open('G:\My Drive\Programming Concepts\Text Dungeon\\' + UserFile, "r") as File:
-                for line in File:
-                    usercheck, passcheck = line.split(',')
-            if UserID == usercheck:
-                if self.UserPass == passcheck:
-                    print("Success")
-                    self.ErrorLabel.config(text='Success')
-                    self.App.destroy()
-                    Game.Username.append(UserID)
-                    Game()
-                else:
-                    print("Wrong Password")
-                    self.ErrorLabel.config(text='Incorrect Password')
-            else:
-                print("User ID Not Found")
-                self.ErrorLabel.config(text='Incorrect Username')
-        except:
-            print('User Not Found')
-            self.ErrorLabel.config(text='User Not Found')
+        try: #opening try loop
+            with open('G:\My Drive\Programming Concepts\Text Dungeon\\' + UserFile, "r") as File: #opening file to read
+                for line in File: #telling the program to look for something for every line in the file
+                    usercheck, passcheck = line.split(',') #split the line at the comma in the files and set them to the 2 variables
+            if UserID == usercheck: #if the entered username matches the username in the file
+                if self.UserPass == passcheck: #if the entered password matches the one in the associated file
+                    print("Success") #print success in the console
+                    self.ErrorLabel.config(text='Success') #change the error message label to read success
+                    self.App.destroy() #close the login application
+                    Game.Username.append(UserID) #assign the username to a list in the Game class
+                    Game() #initialise the game class
+                else: #if none of the above password criteria are met
+                    print("Wrong Password") #error message to console
+                    self.ErrorLabel.config(text='Incorrect Password') #change error message to alert the user
+            else: #if none of the above username criteria are met
+                print("User ID Not Found") #print error message to console
+                self.ErrorLabel.config(text='Incorrect Username') #change error label to alert user
+        except: #if try fails
+            print('User Not Found') #print error message to console
+            self.ErrorLabel.config(text='User Not Found') #alert the user they need to make a profile
 
+##Create Profile Script for New Users
     def RunCreate(self):
-        UserID = self.CreateUsername.get()
-        self.CreatePass = self.CreatePassword.get()
-        self.ConfirmPass = self.ConfirmUsername.get()
-        UserFile = UserID + '.txt'
+        UserID = self.CreateUsername.get() #get the creation username
+        self.CreatePass = self.CreatePassword.get() #get the creation password
+        self.ConfirmPass = self.ConfirmUsername.get() #get the confirmed creation password
+        UserFile = UserID + '.txt' #set a variable to the expected file name
 
-        try: 
-            with open('G:\My Drive\Programming Concepts\Text Dungeon\\' + UserFile, "w") as File:
-                if self.CreatePass == self.ConfirmPass:
-                    print("Success")
-                    File.write(UserID + ',' + self.CreatePass)
-                    self.CreateError.config(text='User Created!')
-                    self.App.destroy()
-                    Game.Username.append(UserID)
-                    Game()
-                else:
-                    print("Passwords Don't Match")
-                    self.CreateError.config(text='Passwords Don\'t Match')
-        except:
-            print('Failed to Create User')
+        try: #open try loop
+            with open('G:\My Drive\Programming Concepts\Text Dungeon\\' + UserFile, "w") as File: #write to a file made with the variable file name
+                if self.CreatePass == self.ConfirmPass: #if the passwords match
+                    print("Success") #print success to console
+                    File.write(UserID + ',' + self.CreatePass) #write the username and password to the specified file
+                    self.CreateError.config(text='User Created!') #change error message to show user creation
+                    self.App.destroy() #close login app
+                    Game.Username.append(UserID) #append username to list in Game class
+                    Game() #initialise game class
+                else: #if passwords don't match
+                    print("Passwords Don't Match") #print error message to console
+                    self.CreateError.config(text='Passwords Don\'t Match') #change error message to alert user
+        except: #if try fails
+            print('Failed to Create User') #print error message to console
 
+##Running Class on File Run
 if __name__ == '__main__':
     Login()
--- Player.py
+++ Player.py
@@ -1,64 +1,103 @@
+##import of existing modules
+import time
+
+##import of classes for purpose of the game
 import Game
 
+##Player Class
 class Player():
 ##A Collection of Variable Lists to Ease Player Selection and to Minimise Code Length in If Statements.
-    North = ['North', 'north', 'N', 'n', 'NORTH']
-    South = ['South', 'south', 'S', 's', 'SOUTH']
-    East = ['East', 'east', 'E', 'e', 'EAST']
-    West = ['West', 'west', 'W', 'w', 'WEST']
-    Yes = ['Yes', 'yes', 'Y', 'y', 'YES']
-    No = ['No', 'no', 'N', 'n', 'NO']
-    Use = ['Use', 'use', 'U', 'u', 'USE']
-    Move = ['Move', 'move', 'M', 'm', 'MOVE']
-    Look = ['Look', 'look', 'L', 'l', 'LOOK']
-    Return = ['Return', 'return', 'R', 'r', 'RETURN']
-    Collect = ['Collect', 'collect', 'C', 'c', 'COLLECT']
-    End = ['End', 'end', 'E', 'e', 'END']
-    Key = ['Key', 'key', 'K', 'k', 'KEY']
-    Bag = ['Bag', 'bag', 'b', 'B', 'BAG']
-    Statistics = ['Statistics', 'statistics', 'S', 's', 'STATISTICS']
-    ArmourChoice = ['Armour', 'armour', 'a', 'A', 'ARMOUR']
-    CoinsChoice = ['Coins', 'coins', 'c', 'C', 'COINS']
-    Open = ['Open', 'open', 'O', 'o', 'OPEN']
+    North = ['North', 'north', 'N', 'n', 'NORTH'] #Command List Moving North
+    South = ['South', 'south', 'S', 's', 'SOUTH'] #Command List Moving South
+    East = ['East', 'east', 'E', 'e', 'EAST'] #Command List Moving East
+    West = ['West', 'west', 'W', 'w', 'WEST'] #Command List Moving West
+    Yes = ['Yes', 'yes', 'Y', 'y', 'YES'] #Command List Saying Yes
+    No = ['No', 'no', 'N', 'n', 'NO'] #Command List Saying No
+    Use = ['Use', 'use', 'U', 'u', 'USE'] #Command List Saying Use
+    Move = ['Move', 'move', 'M', 'm', 'MOVE'] #Command List Saying Move
+    Look = ['Look', 'look', 'L', 'l', 'LOOK'] #Command List Saying Look
+    Return = ['Return', 'return', 'R', 'r', 'RETURN'] #Command List Saying Return
+    Collect = ['Collect', 'collect', 'C', 'c', 'COLLECT'] #Command List Saying Collect
+    End = ['End', 'end', 'E', 'e', 'END'] #Command List Saying End
+    Key = ['Key', 'key', 'K', 'k', 'KEY'] #Command List Saying Key
+    Bag = ['Bag', 'bag', 'b', 'B', 'BAG'] #Command List Saying Bag
+    Statistics = ['Statistics', 'statistics', 'S', 's', 'STATISTICS'] #Command List Saying Statistics
+    ArmourChoice = ['Armour', 'armour', 'a', 'A', 'ARMOUR'] #Command List Saying Armour
+    CoinsChoice = ['Coins', 'coins', 'c', 'C', 'COINS'] #Command List Saying Coins
+    Open = ['Open', 'open', 'O', 'o', 'OPEN'] #Command List Saying Open
 
 ##Players Overall Stats... Including Coin Total, Health Total and Armour Total
-    Coins = 0
-    Health = 100
-    Armour = 0
+    Coins = 0 #Overall Coin Total Variable
+    Health = 100 #Overall Health Total Variable
+    Armour = 0 #Overall Armour Total Variable
 
 ##Player Inventory
-    Inventory = ['Snack']
-    ArmourSet = []
+    Inventory = ['Snack'] #Players Inventory (Also Known as Player Bag)
+    ArmourSet = [] #List Containing Players Armour Collected Through the Game
+    Pet = [] #Secret Feature for Collecting Pets
+    RareEncounter = ['Axolotl']
 
 ##A function to Print the Players Total Statistics
     def TotalStatistics():
-        print('\n\n\n#### Player Stats ####')
+        print('\n\n\n#### Player Stats ####') 
         print('####################')
-        print("Coins: " + str(Player.Coins))
-        print("Health: " + str(Player.Health))
-        print("Armour: " + str(Player.Armour))
-        print("Player Bag: " + str(Player.Inventory))
-        print('Armour Set: ' + str(Player.ArmourSet))
+        print("Coins: " + str(Player.Coins)) #Displays Player Coins Total
+        print("Health: " + str(Player.Health)) #Displays Players Health Total
+        print("Armour: " + str(Player.Armour)) #Displays Player Armour Total
+        print("Player Bag: " + str(Player.Inventory)) #Shows Current Inventory
+        print('Armour Set: ' + str(Player.ArmourSet)) #Shows Current Armour
         print('\n\n\n')
 
 ##A Function to Print the Players Bag at Any Time the Function is Called
     def PlayerBag():
         print('\n\n\n#### Player Bag ####')
         print('##################')
-        print(str(Player.Inventory) + '\n\n\n')
+        print(str(Player.Inventory) + '\n\n\n') #Shows Current Inventory
 
 ##A Function to Allow the Player to Collect Armour, Used Cross Class To prevent Lenghty Game Code
     def ArmourCollect(Type, Weight):
-        Player.ArmourSet.append(Type)
-        Player.Armour += Weight
-        print('Your Armour is Now: ' + str(Player.Armour))
+        Player.ArmourSet.append(Type) #Appends Armour Type to the Armour List
+        Player.Armour += Weight #Adds Armour Strength to the Armour Variable
+        print('Your Armour is Now: ' + str(Player.Armour)) #Displays the Current Armour Variable
 
 ##A Function to Allow Players to Collect Items Into Their Inventory, Used Cross Class to prevent Lengthy Game Code
     def ItemCollect(Type):
-        Player.Inventory.append(Type)
-        print(Player.PlayerBag())
+        Player.Inventory.append(Type) #Appends Items to the Players Bag
+        print(Player.PlayerBag()) #Displays Player Bag Function
 
 ##A Function Used to Collect Coins to Add to Their Score, Used Cross Class to Prevent Lengthy Game
     def CoinCollect(Amount):
-        Player.Coins += Amount
-        print('You Now Have ' + str(Player.Coins) + ' Coins...')
+        Player.Coins += Amount #Adds Specified Number of Coins
+        print('You Now Have ' + str(Player.Coins) + ' Coins...') #Displays Coins
+
+##Shows Current Pets, Only Used In End Game to Remain Secret Feature
+    def PetCheck():
+        print('\n\n\n#### Pets ####')
+        print('##################')
+        print(str(Player.Pet) + '\n\n\n') #Displays Current Pets
+
+#Secret Pet Encounter in Room2 Desk Drawer
+    def RarePetEncounter():
+        if 'Axolotl' in Player.RareEncounter: #only run following code if don't have axolotl
+            Brave = input('Do You Dare Open the Draw?\n[Yes] or [No]\n> ') #Player Input to Enter Drawer
+            if Brave in Player.Yes: #If Statement for Yes Select
+                print('You See Something Small Moving Around in the Drawer') #Scene Setting
+                time.sleep(2) #Sleep Feature, adds Suspense
+                Arfa = input('It\'s a Small Axolotl, Would You Like to Take Him?\n[Yes] or [No]\n> ') #input to collect pet
+                if Arfa in Player.Yes: #if statement for pet collection
+                    Player.Pet.append('Axolotl') #appends axolotl to the Pet List
+                    print('You Acquired an Axolotol as a Pet!') #Informs Player of Action that happened
+                elif Arfa in Player.No: #If Player Chooses No and irs in list
+                    print('You Chose Not to Collect the Axolotl...') #informing player of choice
+                    time.sleep(2) #sleep feature, adds suspense
+                    print('You\'re returning to the desk...') #informing player of next move
+                    Game.Game.Room2Desk() #returning to desk
+            elif Brave in Player.No: #if statement for not checking drawer
+                print('You Chose Not to Check...') #informing user of choice
+                Game.Game.Room2Desk() #Back to Desk
+            else: #final else
+                print('Please Choose a Valid Choice...') #informing of invalid choice
+                Player.RarePetEncounter() #runs section of code again from choice
+        else: #already have axolotl so ignore if statement
+            print('Maybe You Were Just Seeing Things...') #inform player choice means nothing
+            Game.Game.Room2Desk() #back to the desk
